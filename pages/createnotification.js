4c77d7848ce292e46ffa1fc267475af6e30e4f9c
MaidSimpl <panjwani1996@gmail.com>
2023-11-02 11:38:25 +0500

    favicon

--- pages/createnotification.js
+++ pages/createnotification.js
@@ -6,10 +6,10 @@ const createnotification = () => {
   return (
     <>
       <div className="w-full flex bg-[#FAFAFA]">
-        <div className="w-[20%] border border-r-2 border-[#d9d9d9]">
+        <div className="w-[23%] border border-r-2 border-[#d9d9d9]">
           <Sidebar />
         </div>
-        <div className="w-[80%]">
+        <div className="w-[77%]">
           <CreateNotification />
         </div>
       </div>
